11121ed15864f931d5fe61a246f4d1877acef999
vanlandschootsh900 <vanlandschootsh900@northwested.org>
2026-03-10 10:45:11 -0400

    w

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+//Shay VanLandschoot
+//09 Mar 2026
+//Variable Declaration & Assignment 
 
+const firstName = 'Shay';
+const lastName = 'Vanlandschoot';
+const eyeColor = 'green';
+const sendingSchool = 'traverse city centerl high school';
+const careerTechProgram = 'Web and App devlement';
+const grade = 12;
+
+
+console.log(firstName);
+console.log(lastName);
+console.log(eyeColor);
+console.log(sendingSchool);
+console.log(careerTechProgram);
+console.log(grade);
