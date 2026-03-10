2aff170628cc0ec855e0bfa1ee2f5a83016ca1bd
vanlandschootsh900 <vanlandschootsh900@northwested.org>
2026-03-10 11:20:41 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,11 +8,28 @@ const eyeColor = 'green';
 const sendingSchool = 'traverse city centerl high school';
 const careerTechProgram = 'Web and App devlement';
 const grade = 12;
+const quiz1 = 75
+const quiz2 = 70
+const quiz3 = 100
+
+let avgQuizScore = (quiz1 + quiz2 + quiz3)/3;
+
+const display = `
+Name: ${firstName} ${lastName}
+Sending School: ${sendingSchool}
+CTC Program: ${careerTechProgram}
+Grade: ${grade}
+avg Quiz Scores: ${avgQuizScore}
+
+`;
+
+console.log(display)
 
 
-console.log(firstName);
-console.log(lastName);
-console.log(eyeColor);
-console.log(sendingSchool);
-console.log(careerTechProgram);
-console.log(grade);
+// console.log(firstName);
+// console.log(lastName);
+// console.log(eyeColor);
+// console.log(sendingSchool);
+// console.log(careerTechProgram);
+// console.log(grade);
+// console.log(avgQuizScore);
